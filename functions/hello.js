b1f53c75e88d62e17c8ed16eb4fc3a10d925da96
Chris Ward <veggie2u@cyberward.net>
2020-04-14 22:32:41 -0500

    add hello function

---
+++ functions/hello.js
@@ -0,0 +1,11 @@
+/**
+ * Netlify function that returns Hello World or a name parameter
+ */
+exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || "World";
+
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`
+  };
+};
