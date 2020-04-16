6af6cd236928ecc931c73f05e4b84ad0a08259a6
Chris Ward <veggie2u@cyberward.net>
2020-04-15 21:27:17 -0500

    trying to run funcion with import

--- functions/fetchGroup.js
+++ functions/fetchGroup.js
@@ -4,7 +4,8 @@ import fetch from "node-fetch";
  * Netlify function that returns feeds from a group from AIO
  */
 const { AIO_KEY, AIO_URL } = process.env;
-
+console.log(AIO_URL);
+console.log(process.env);
 exports.handler = async (event, context) => {
   return fetch(AIO_URL, { headers: { "Accept": "application/json", 'X-AIO-Key': AIO_KEY } })
     .then(response => response.json())
