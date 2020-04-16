5a45694734a9e709f74fc712318378671da2e777
Chris Ward <veggie2u@cyberward.net>
2020-04-15 19:06:00 -0500

    add fetch group function

---
+++ functions/fetchGroup.js
@@ -0,0 +1,15 @@
+/**
+ * Netlify function that returns feeds from a group from AIO
+ */
+import fetch from "node-fetch";
+const { AIO_KEY, AIO_URL } = process.env;
+
+exports.handler = async (event, context) => {
+  return fetch(AIO_URL, { headers: { "Accept": "application/json", 'X-AIO-Key': AIO_KEY } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+    .catch(error => ({ statusCode: 500, body: String(error) }));
+};
