3862d2936b0529592d739c988562f71cc9ac30f9
Chris Ward <veggie2u@cyberward.net>
2020-04-19 16:11:09 -0500

    Trying function redirect

--- src/components/FetchTemp.js
+++ src/components/FetchTemp.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { parseISO, format } from 'date-fns';
-import config from "../aio_config.json";
+import TempCard from "./TempCard";
 
 const Fetch = () => {
   const [data, setData] = useState({ feeds: [] });
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-AIO-Key': config.aioKey
-    }
-  }
-  const url = config.feedUrl;
-  
+
+  const url = '/api/fetchGroup';
+
   useEffect(() => {
     const fetchData = async () => {
-      let res = await fetch(url, options);
+      let res = await fetch(url);
       let response = await res.json();
       setData(response);
     };
@@ -23,15 +17,21 @@ const Fetch = () => {
     // eslint-disable-next-line
   }, []);
   return (
-    <table>
-      <tbody>
+    <React.Fragment>
       {data.feeds.map(feed => (
-        <tr key={feed.key}>
-          <td>{feed.name}</td><td>{feed.last_value}</td><td>{format(parseISO(feed.updated_at), 'MM/dd hh:mm:ss a')}</td>
-        </tr>
+        <TempCard />
       ))}
-      </tbody>
-    </table>
+    
+      <table>
+        <tbody>
+        {data.feeds.map(feed => (
+          <tr key={feed.key}>
+            <td>{feed.name}</td><td>{feed.last_value}</td><td>{format(parseISO(feed.updated_at), 'MM/dd hh:mm:ss a')}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 };
 
